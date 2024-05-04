d17d231d53605fcc53a86f16f46ca1ecd978a1cc
potatoesAreGod <118043038+potatoesAreGod@users.noreply.github.com>
2024-05-04 10:56:41 +0200

    moved fetchsmhi to new file

--- Program.cs
+++ Program.cs
@@ -1,54 +1,13 @@
-﻿using System.Text.Json;
-
-class Program
+﻿namespace weathrForecastr
 {
-    static async Task FetchSmhi()
+    class Program
     {
-        double lat = 58;
-        double lon = 16;
-
-        string apiUrl = $"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{lon}/lat/{lat}/data.json";
-
-        HttpClient client = new();
-
-        try
+        static void Main()
         {
-            // Make request
-            HttpResponseMessage response = await client.GetAsync(apiUrl);
-            response.EnsureSuccessStatusCode();
-
-            // Print headers
-            var headers = response.Headers;
-            Console.WriteLine("Headers:");
-            foreach (var header in headers)
-            {
-                Console.WriteLine($"{header.Key}: {string.Join(", ", header.Value)}");
-            }
-
-            // Read JSON
-            string jsonData = await response.Content.ReadAsStringAsync();
-            var jsonDataObject = JsonDocument.Parse(jsonData);
-            Console.WriteLine("JSON data:");
-            Console.WriteLine(jsonData);
-
-            if (jsonDataObject.RootElement.TryGetProperty("t", out JsonElement element))
-            {
-                Console.WriteLine("T value: {0}", element);
-            }
-        } catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Weathr Forecastr");
+            SmhiForecast.FetchSmhi();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
-        finally
-        {
-            client.Dispose();
-        }
-    }
-
-    static void Main()
-    {
-        Console.WriteLine("Weathr Forecastr");
-        FetchSmhi();
-        Console.ReadKey();
     }
 }
